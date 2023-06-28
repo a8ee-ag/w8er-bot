96b374777a76c53e9a1ece1277c725b335b5b97c
Артём <a8ee@MacBook-Pro-Artem-2.local>
2023-06-28 14:29:54 +0300

    next g

--- hier_herr_w8er_bot.py
+++ hier_herr_w8er_bot.py
@@ -1,3 +1,50 @@
-import os
+import logging
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, InlineQueryHandler
 
-print (3+5)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Ну привет что ли")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Напиши нормальную команду...")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('5939815261:AAEIx_QxWyJnCAT4t0wB67b_ri8zdcHBCjw').build()
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    start_handler = CommandHandler('start', start)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(unknown_handler)
+    
+    application.run_polling()
