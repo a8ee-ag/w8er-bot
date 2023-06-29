15221a6d335d11754f68047a02f7d98fbdfcb189
Артём <a8ee@MacBook-Pro-Artem-2.local>
2023-06-29 12:11:27 +0300

    .....

--- hier_herr_w8er_bot.py
+++ hier_herr_w8er_bot.py
@@ -1,6 +1,6 @@
 import pytz
 import locale
-from datetime import datetime
+from datetime import datetime, timedelta
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, InlineQueryHandler
 ########################################################################################################################
@@ -12,18 +12,25 @@ async def gay(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def date(update: Update, context: ContextTypes.DEFAULT_TYPE):
     tz = pytz.timezone('Europe/Moscow')
     now = datetime.now(tz)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text = now.strftime("Во ты дурик, дату сегодняшнюю забыл, ладно, напомню" "\n" "%d %B %Y (Если ты совсем потерялся, то сегодня %A)"))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = now.strftime("Во ты дурик, дату сегодняшнюю забыл, ладно, напомню" "\n" "%d %B %Y года (Если ты совсем потерялся, то сегодня %A)"))
 ########################################################################################################################
 async def hh(update: Update, context: ContextTypes.DEFAULT_TYPE):
     hh = datetime(2023, 6, 29, 3, 1, 0)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text = hh.strftime("Уехал в армию %d %B %Y в %H:%M, это был затянутый тучами депрессивный %A"))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = hh.strftime("Уехал в армию %d %B %Y года в %H:%M, это был затянутый тучами депрессивный %A"))
 ########################################################################################################################
 async def nap(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text = " «Мое вам последнее напутствие." "\n" "В New Vegas и 3 фолыче можно толкать браминов, нажав на них клавишу действия, находясь в режиме скрытности»" "\n" "© Товарищ hier Herr ")
 ########################################################################################################################
 async def ost(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    await context.bot.send_message(chat_id=update.effective_chat.id, text =)
+    tz = pytz.timezone('Europe/Moscow')
+    now = datetime.now(tz)
+    hh = tz.localize(datetime(2023, 6, 29, 3, 1, 0))
+    diff = now - hh
+    days = diff.days
+    hours = diff.seconds//3600
+    min = (diff.seconds//60)%60
+    sec = diff.seconds%60
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = "Товарищ уехал в армию уже как {} дней {} часов {} минут {} секунд".format(days, hours, min, sec))
 ########################################################################################################################
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Напиши нормальную команду...")
@@ -32,12 +39,12 @@ if __name__ == '__main__':
     with open ('token.txt', 'r') as file:
         token = file.read().strip()
     application = ApplicationBuilder().token(token).build()
-    
+
     gay_handler = CommandHandler('itsokaytobegay', gay)
     date_handler = CommandHandler('date', date)
     hh_handler = CommandHandler('hh', hh)
-    nap_handler = CommandHandler('naputstvie', nap)
-    ost_handler = CommandHandler('ostalos', ost)
+    nap_handler = CommandHandler('nap', nap)
+    ost_handler = CommandHandler('ost', ost)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(gay_handler)
