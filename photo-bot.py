b898565e35fc1b2f52bae2cf804c8a6a824c3233
Артём <a8ee@MacBook-Pro-Artem-2.local>
2023-07-10 09:00:05 +0300

    qwerty

--- photo-bot.py
+++ photo-bot.py
@@ -13,21 +13,21 @@ async def re_msg(update: Update, context: CallbackContext):
     while True:
         n = random.randint(1, 12)
         n = str(n)
-        m = random.randint(1, 16)
+        m = random.randint(1, 17)
         m = str(m)
         message = "txt/" + m + ".txt"
         img_path = "img/rnd_igm/" + n + ".jpg"
         with open (message, "r") as msg_path:
             msg = msg_path.read().strip()
-        with open ('tokens/token.txt', 'r') as file:
+        with open ('tokens/token2.txt', 'r') as file:
             token = file.read().strip()
         bot = Bot(token)
         await context.bot.send_photo(chat_id=update.effective_chat.id, caption = msg, photo = open(img_path, "rb"))
-        time.sleep(1*60*60)
+        time.sleep(1*1*15)
         
 
 if __name__ == '__main__':
-    with open ('tokens/token.txt', 'r') as file:
+    with open ('tokens/token2.txt', 'r') as file:
         token = file.read().strip()
     application = ApplicationBuilder().token(token).build()
 
